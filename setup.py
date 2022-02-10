c847929833a35d88e46dbc51d163700f27c64cff
Kailash <ahikailash1@gmail.com>
2022-02-11 00:33:17 +0530

    Version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ravftp",
-    version="0.1-alpha",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         "pyftpdlib==1.5.6"
