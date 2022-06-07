f65da3796f691320e9d4104d30adcbf17e42f8dc
Kailash <ahikailash1@gmail.com>
2022-06-07 19:27:29 +0530

    The script to start the flask server is updated

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ravftp",
-    version="0.4",
+    version="0.4.5",
     license='MIT',
     author="Raven Protocol",
     author_email='kailash@ravenprotocol.com',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ravenprotocol/ravftp',
     keywords='Ravftp, ftp server, flask server',
     install_requires=[
